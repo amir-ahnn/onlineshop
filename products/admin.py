2d1ab46a779467bb5d5b77c53a4156797680a8f6
amirhosein <amir.ahadnezhaad@gmail.com>
2022-09-16 17:03:01 +0430

    add translation to products app

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active']
+    extra = 0
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created']
+
+    inlines = [
+        CommentInLine,
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'body', 'stars', 'active']
