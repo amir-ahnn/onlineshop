21f33faa94098f1675fe386b06b8f5c23408c2da
amirhosein <amir.ahadnezhaad@gmail.com>
2022-11-13 19:11:38 +0330

    add product image field

--- products/models.py
+++ products/models.py
@@ -4,11 +4,13 @@ from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from django.utils.translation import  gettext_lazy as _
 
+
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
+    image = models.ImageField(verbose_name=_('Product Image'), upload_to='products/product_cover', blank=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
